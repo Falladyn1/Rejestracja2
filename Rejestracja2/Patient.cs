25ec6752b09d506c3e5c1ebf6938eb547e05f954
Falladyn1 <matibati300@gmail.com>
2026-08-18 22:10:57 +0200

    Klasa pacjenta i baza danych

---
+++ Rejestracja2/Patient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rejestracja2
+{
+    public class Patient
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Pesel { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public Patient(string firstName, string lastName, string pesel, string phoneNumber, string address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Pesel = pesel;
+            PhoneNumber = phoneNumber;
+            Address = address;
+        }
+    }
+}
---
+++ Rejestracja2/PatientDatabase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rejestracja2
+{
+    public static class PatientDatabase
+    {
+        private static readonly string filePath = "patients.txt";
+
+        public static List<Patient> LoadPatients()
+        {
+            List<Patient> patients = new List<Patient>();
+
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+            }
+
+            foreach (string line in File.ReadAllLines(filePath))
+            {
+                string[] data = line.Split(';');
+
+                if (data.Length == 5)
+                {
+                    patients.Add(new Patient(data[0], data[1], data[2], data[3], data[4]));
+                }
+            }
+
+            return patients;
+        }
+
+        public static void SavePatients(List<Patient> patients)
+        {
+            List<string> lines = new List<string>();
+
+            foreach (Patient patient in patients)
+            {
+                lines.Add($"{patient.FirstName};{patient.LastName};{patient.Pesel};{patient.PhoneNumber};{patient.Address}");
+            }
+
+            File.WriteAllLines(filePath, lines);
+        }
+    }
+}
